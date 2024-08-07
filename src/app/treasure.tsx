d99155b4d6a4ee63f96d572aa9dd230661c59956
Winsor <winsoromar12@gmail.com>
2024-08-06 20:24:20 -0400

    feat: add treasure component

---
+++ src/app/treasure.tsx
@@ -0,0 +1,14 @@
+export default function Treasure() {
+  return (
+    <div className="chest">
+      <div className="insert"></div>
+      <div className="top"></div>
+      <div style={{ width: "100%", position: "relative" }}>
+        <div className="chest_keyholeBG">
+          <div className="chest_keyhole"></div>
+        </div>
+        <div className="bottom"></div>
+      </div>
+    </div>
+  );
+}
